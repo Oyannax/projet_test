b267caaee4b4433e037a6a6120bfb0314baaa4a8
Oyannax <oyannalopez14@gmail.com>
2024-07-24 11:35:24 +0200

    inital commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Mon Projet</title>
+</head>
+<body>
+    <h1>Bienvenue sur mon projet!</h1>
+    <?php
+        echo "<p>Ceci est un paragraphe généré par PHP.</p>";
+    ?>
+    <script src="script.js"></script>
+</body>
+</html>
